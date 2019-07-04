06e1ed6a8a2bd73c4532e36890378cbbb0430472
ykxD <waiyongkang1999@gmail.com>
2019-07-05 01:47:40 +0800

    Signed-off-by: ykxD <waiyongkang1999@gmail.com>

--- app/src/main/java/com/example/letseat1/MainActivity.java
+++ app/src/main/java/com/example/letseat1/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.letseat1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        loginButton = findViewById(R.id.loginBtn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/letseat1/SignUp.java
@@ -0,0 +1,13 @@
+package com.example.letseat1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
